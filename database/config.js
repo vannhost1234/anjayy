c414e9e898d437b0f30f8f867b3b7ecbbb2e4db4
vannhost1234 <vannhost1234@users.noreply.github.com>
2026-01-20 19:19:10 +0700

    🔥 Tools By Vann Hosting 🔥

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8198417165:AAGRdppBnltMwvGRDxeC1TeUVZMamLMHoUc",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7517485937", // Ubah Jadi Id Mu !!!
+  port: "3000", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "0.0.0.0" // Ubah Jadi Ip Vps Mu !!!
+};
